47bb7ef6e9c4d4f7c7187463a3896f1c566795cb
durmusrasit <mrdurmus@outlook.com>
2022-05-15 01:09:42 +0300

    Check if members have a specific role, and if the role is not present in the member, the role is added to the member. The role name was moved to the env file and the names of the variables were changed.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,10 +2,10 @@ module.exports = {
   name: "guildMemberAdd",
   execute(member) {
     // console.log("guildMemberAdd event", member.displayName);
-    const roleName = "Member";
-    let role = member.guild.roles.cache.find((r) => r.name === roleName);
+    const roleName = process.env.ROLE;
+    let memberRole = member.guild.roles.cache.find((r) => r.name === roleName);
 
     console.log('GIVE "' + roleName + '" ROLE -> ' + member.displayName);
-    member.roles.add(role).catch(console.error);
+    member.roles.add(memberRole).catch(console.error);
   },
 };
--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,20 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+
+    const guild = client.guilds.cache.first();
+    const roleName = process.env.ROLE;
+    const memberRole = guild.roles.cache.find((role) => role.name === roleName);
+
+    guild.members.cache.forEach((member) => {
+      const hasRole = member.roles.cache.some(
+        (role) => role.id === memberRole.id
+      );
+
+      if (!hasRole) {
+        console.log('GIVE "' + roleName + '" ROLE -> ' + member.displayName);
+        member.roles.add(memberRole).catch(console.error);
+      }
+    });
   },
 };
