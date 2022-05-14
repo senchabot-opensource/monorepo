620a027c8ce82b493254c6a5c6f0233f5895b095
durmusrasit <mrdurmus@outlook.com>
2022-05-14 18:41:25 +0300

    Add role to member on guildMemberAdd

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,5 +2,10 @@ module.exports = {
   name: "guildMemberAdd",
   execute(member) {
     // console.log("guildMemberAdd event", member.displayName);
+    const roleName = "Member";
+    let role = member.guild.roles.cache.find((r) => r.name === roleName);
+
+    console.log('GIVE "' + roleName + '" ROLE -> ' + member.displayName);
+    member.roles.add(role).catch(console.error);
   },
 };
