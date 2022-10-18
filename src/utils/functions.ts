6151390571084890b6a41a2a117922f03f47a17f
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2022-10-18 23:17:09 +0300

    Add capitalizeWord function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -7,3 +7,8 @@ export function isInvalidColorCode(color: string) {
     return styleOption.color === "" && true;
   }
 }
+
+export function capitalizeWord(word: string) {
+  const capitalizedChar = word.charAt(0).toLocaleUpperCase();
+  return capitalizedChar + word.slice(1);
+}
