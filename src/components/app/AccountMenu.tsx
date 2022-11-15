1d5104b5a1b9a2e5ef14e5ca5a3b6cd0147ff644
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2022-11-16 02:39:27 +0300

    Move Menu PaperProps and AppBar styles to styles & update landing appbar

--- src/components/app/AccountMenu.tsx
+++ src/components/app/AccountMenu.tsx
@@ -15,6 +15,7 @@ import Settings from "@mui/icons-material/Settings";
 import Logout from "@mui/icons-material/Logout";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import { MenuPaperPropsStyles } from "../../styles";
 
 export const AccountMenu = () => {
   const { data: session } = useSession();
@@ -60,37 +61,7 @@ export const AccountMenu = () => {
         open={open}
         onClose={handleClose}
         onClick={handleClose}
-        PaperProps={{
-          elevation: 1,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px, 2px, 8px rgba(0,0,0,0.32))",
-            boxShadow:
-              "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
-            backdropFilter: "blur(5px)",
-            backgroundColor: "inherit",
-            mt: 1,
-
-            "& .MuiAvatar-root": {
-              width: 28,
-              height: 28,
-              ml: -0.5,
-              mr: 1.5,
-            },
-            /*"&:before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },*/
-          },
-        }}
+        PaperProps={MenuPaperPropsStyles}
         transformOrigin={{ horizontal: "right", vertical: "top" }}
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
--- src/components/landing/LandingAppBar.tsx
+++ src/components/landing/LandingAppBar.tsx
@@ -5,28 +5,49 @@ import {
   Typography,
   alpha,
   IconButton,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import Link from "next/link";
 //import LandingButton from "./LandingButton";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useSession } from "next-auth/react";
 import { Offset } from "../Offset";
 import { env } from "../../env/client.mjs";
 import AppBarTitle from "../common/AppBarTitle";
+import LandingButton from "./LandingButton";
+import { AppBarStyles, MenuPaperPropsStyles } from "../../styles";
+import React from "react";
+
+const appBarMenuList = [
+  { title: "Cookie Policy", path: "/cookie-policy" },
+  { title: "Privacy Policy", path: "/privacy-policy" },
+  { title: "Terms of Service", path: "/terms" },
+  { title: "Open Source Licenses", path: "/licenses" },
+];
 
 const LandingAppBar = () => {
   const { data: session } = useSession();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
     <>
       <AppBar
         position="sticky" // adds pb: 8
         color="transparent"
-        sx={{
-          backdropFilter: "blur(1px)",
-          backgroundColor: alpha("#000", 0.75),
-        }}
+        sx={AppBarStyles}
         elevation={0}
       >
         <Box sx={{ flexGrow: 1 }}>
@@ -36,30 +57,51 @@ const LandingAppBar = () => {
               userSelect: "none",
             }}
           >
+            <LandingButton
+              sx={{
+                pr: 4,
+                display: { xs: "flex", md: "none" },
+                color: "#646464",
+              }}
+              onClick={handleOpenNavMenu}
+              disableRipple
+            >
+              <MenuIcon sx={{ backgroundColor: "#000" }} />
+            </LandingButton>
+            <Menu
+              id="landing-menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+              keepMounted
+              transformOrigin={{ vertical: "top", horizontal: "left" }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+              PaperProps={MenuPaperPropsStyles}
+            >
+              {appBarMenuList.map((item, index) => (
+                <Link href={item.path}>
+                  <MenuItem key={index}>
+                    <Typography textAlign="center">{item.title}</Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </Menu>
             <AppBarTitle />
 
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              {/*<Link href="/cookie-policy">
-                <LandingButton sx={{ mr: 2, color: "#646464" }} disableRipple>
-                  Cookie Policy
-                </LandingButton>
-              </Link>
-
-              <Link href="/privacy-policy">
-                <LandingButton sx={{ mr: 2, color: "#646464" }} disableRipple>
-                  Privacy Policy
-                </LandingButton>
-              </Link>
-              <Link href="/terms">
-                <LandingButton sx={{ mr: 2, color: "#646464" }} disableRipple>
-                  Terms of Service
-                </LandingButton>
-              </Link>
-              <Link href="/licenses">
-                <LandingButton sx={{ mr: 10, color: "#646464" }} disableRipple>
-                  Open Source Licenses
-                </LandingButton>
-              </Link>*/}
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", sm: "none", md: "flex" },
+              }}
+            >
+              {appBarMenuList.map((item, index) => (
+                <Link href={item.path}>
+                  <LandingButton sx={{ mr: 2, color: "#646464" }} disableRipple>
+                    {item.title}
+                  </LandingButton>
+                </Link>
+              ))}
             </Box>
 
             <Link href="/app">
