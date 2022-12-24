ab5a32056eec38f9364c7955178859e7c9fabc01
durmusrasit <mrdurmus@outlook.com>
2022-12-25 01:04:09 +0300

    Update voiceStateUpdate event & add methods and checks. Add getChannelIndex method. Add getChannel method. add removeChannel method. Add checks to ensure that the channel exists before modifying or accessing its properties. Implement voiceStateUpdate event to delete empty voice channels.

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,6 +1,31 @@
+const { dynamicVoice } = require("../utils/dynamicVoice");
+
 module.exports = {
   name: "voiceStateUpdate",
-  execute(oldState, newState) {
-    // console.log("oldState", oldState, "newState", newState);
+  async execute(oldState, newState) {
+    try {
+      // Fetch all channels in the guild
+      const channels = await newState.guild.channels.fetch();
+
+      // Iterate through channels
+      channels.some((_channel) => {
+        // Get the number of members in the channel
+        const memberCount = [..._channel.members].length;
+
+        const channelId = _channel.id;
+        const channel = dynamicVoice.getChannel(channelId);
+
+        // If the channel exists and the number of members in the channel is less than 1, delete the channel
+        if (channel && memberCount < 1) {
+          // Remove the channel from the dynamicVoice object
+          dynamicVoice.removeChannel(channelId);
+          // Delete the channel
+          _channel.delete();
+        }
+      });
+    } catch (e) {
+      // Log any errors that occur
+      console.log("ERROR on voiceStateUpdate:", e);
+    }
   },
 };
--- utils/dynamicVoice.js
+++ utils/dynamicVoice.js
@@ -3,7 +3,7 @@ class DynamicVoice {
 
   /**
    * Get the list of channels
-   * @returns The list of channels
+   * @return {Array} The list of channels
    */
   getChannels() {
     return this.channels;
@@ -11,16 +11,30 @@ class DynamicVoice {
 
   /**
    * Add a new channel to the list of channels
-   * @param {*} channel The channel data to be saved
+   * @param {Object} channel The channel data to be saved
    */
   addChannel(channel) {
+    if (!channel || !channel.channelId) {
+      throw new Error("Invalid channel data");
+    }
     this.channels.push({ ...channel, createdAt: Date.now() });
   }
 
   /**
+   * Returns the channel with the given ID
+   * @param {String} channelId
+   * @returns The channel object, or undefined if not found
+   */
+  getChannel(channelId) {
+    const channelIndex = this.getChannelIndex(channelId);
+
+    return this.channels[channelIndex];
+  }
+
+  /**
    * Check if the user has any channels in the list
-   * @param {*} userId The ID of the user
-   * @returns `true` if the user has at least one channel, `false` otherwise
+   * @param {String} userId The ID of the user
+   * @return {Boolean} `true` if the user has at least one channel, `false` otherwise
    */
   userHasChannels(userId) {
     return this.channels.some((channel) => channel.ownerId === userId);
@@ -28,11 +42,37 @@ class DynamicVoice {
 
   /**
    * Check if there is a channel with the given name in the list
-   * @param {*} channelName The name of the channel
-   * @returns `true` if there is a channel with the given name, `false` otherwise
+   * @param {String} channelName The name of the channel
+   * @return {Boolean} `true` if there is a channel with the given name, `false` otherwise
    */
   channelNameInUse(channelName) {
     return this.channels.some((channel) => channel.channelName === channelName);
+  }
+
+  /**
+   * Get the index of the channel with the given ID in the list
+   * @param {String} channelId
+   * @return {Number} The index of the channel, or -1 if it is not found
+   */
+  getChannelIndex(channelId) {
+    return this.channels.findIndex(
+      (channel) => channel.channelId === channelId
+    );
+  }
+
+  /**
+   * Remove the channel with the given ID from the list
+   * @param {String} channelId The ID of the channel to be removed
+   */
+  removeChannel(channelId) {
+    const channelIndex = this.getChannelIndex(channelId);
+
+    if (channelIndex === -1) {
+      throw new Error(`Channel with ID ${channelId} not found`);
+    }
+
+    // Remove the channel from the list
+    this.channels.splice(channelIndex, 1);
   }
 }
 
