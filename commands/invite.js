341e2d6997562734e93827108a9dbd43eea50b90
durmusrasit <mrdurmus@outlook.com>
2022-12-27 20:36:49 +0300

    Update invite application command Refactor OAuth2 URL construction for clarity and maintainability

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,9 @@ module.exports = {
     .setName("invite")
     .setDescription("Senchabot invite url."),
   execute(interaction) {
-    const url = `https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENTID}&permissions=302058496&scope=bot%20applications.commands`;
+    const client_id = process.env.CLIENTID;
+    const permissions = 302058496;
+    const url = `https://discord.com/api/oauth2/authorize?client_id=${client_id}&permissions=${permissions}&scope=bot%20applications.commands`;
 
     interaction.reply({ content: url, ephemeral: true });
   },
