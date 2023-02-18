5500ab166a738c516e87a760070f2dcba8560265
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-02-18 14:24:38 +0300

    Use join for command path

--- handlers/commandHandler.ts
+++ handlers/commandHandler.ts
@@ -1,15 +1,18 @@
 import { readdirSync } from "fs";
 import { IDiscordClient } from "../client";
+import { join } from "path";
 
-export default (client: IDiscordClient) => {
+export default async (client: IDiscordClient) => {
   console.log("INITIALIZING COMMAND HANDLER");
 
-  const commandFiles = readdirSync("./commands/").filter((file: any) =>
+  const commandsPath = join(__dirname, "../commands");
+  const commandFiles = readdirSync(commandsPath).filter((file: any) =>
     file.endsWith(".ts")
   );
 
   for (const file of commandFiles) {
-    const command = require(`../commands/${file}`);
+    const filePath = join(commandsPath, file);
+    const command = await import(filePath);
 
     if (command.default.data) {
       client.commands.set(command.default.data.name, command.default);
