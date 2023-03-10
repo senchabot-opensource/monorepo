f3a9a0c5aed0cf9b374b96dfa6e91738dbe4c608
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-03-10 19:22:34 +0300

    Add octoHandler class

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+export interface IOctoOptions {
+  auth: string | undefined;
+}
+
+export interface IIssueParams {
+  state: "open" | "closed" | "all" | undefined;
+  sort: "created" | "updated" | "comments" | undefined;
+}
