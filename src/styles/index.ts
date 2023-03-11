f629790aaa26ad3d0543ccd77b18052d05e2dd0e
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-03-11 21:09:09 +0300

    Add alpha color to menu paper background

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -13,7 +13,7 @@ export const MenuPaperPropsStyles = {
     boxShadow:
       "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
     backdropFilter: "blur(5px)",
-    backgroundColor: "inherit",
+    backgroundColor: alpha("#000", 0.9),
     mt: 1,
 
     "& .MuiAvatar-root": {
