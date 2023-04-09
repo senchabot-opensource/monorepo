ad366b9ea646d3cc44bf173c8aa74ad2478c33f6
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-04-09 14:24:17 +0300

    chore(apps/reflect): Remove unused lines in Breadcrumb component

--- apps/reflect/src/components/app/Breadcrumb.tsx
+++ apps/reflect/src/components/app/Breadcrumb.tsx
@@ -1,16 +1,6 @@
 import { Breadcrumbs, Link as MuiLink } from "@mui/material";
 import Link from "next/link";
-/*import WhatshotIcon from "@mui/icons-material/Whatshot";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import SettingsIcon from "@mui/icons-material/Settings";
-*/ import { useRouter } from "next/router";
-
-/*function checkAttributes(pathLength: number, index: number) {
-  if (pathLength - 1 === index) {
-    return { "aria-current": "page" };
-  }
-  return null;
-}*/
+import { useRouter } from "next/router";
 
 const Breadcrumb = () => {
   const router = useRouter();
@@ -18,22 +8,16 @@ const Breadcrumb = () => {
   const pathNames = pathName.split("/").slice(1);
   const pathLength = pathNames.length;
 
-  // const nonWordPattern = /\W/g;
-  // const pathDepth = pathName.match(nonWordPattern);
-  // console.log("pathDepth", pathDepth);
-
   return (
     <Breadcrumbs aria-label="breadcrumb" sx={{ ml: 1 }}>
       {pathNames.map((path, index) => (
         <Link
           key={index}
-          href={`${pathLength - 1 === index ? pathName : "/" + path}`}
-        >
+          href={`${pathLength - 1 === index ? pathName : "/" + path}`}>
           <MuiLink
             underline="hover"
             sx={{ display: "flex", alignItems: "center" }}
-            color={pathLength - 1 === index ? "text.primary" : "inherit"}
-          >
+            color={pathLength - 1 === index ? "text.primary" : "inherit"}>
             {path.charAt(0).toLocaleUpperCase() + path.slice(1)}
           </MuiLink>
         </Link>
