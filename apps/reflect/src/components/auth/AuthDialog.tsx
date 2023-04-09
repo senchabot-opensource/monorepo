9ea33d7fcff0e676a87812f117581d6c8bafa9dc
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-04-09 14:38:10 +0300

    chore(apps/reflect): Remove unused lines in AuthDialog component

--- apps/reflect/src/components/auth/AuthDialog.tsx
+++ apps/reflect/src/components/auth/AuthDialog.tsx
@@ -1,16 +1,8 @@
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import {
-  Dialog,
-
-  //alpha,
-} from "@mui/material";
-
-import { useRouter } from "next/router";
+import { Dialog } from "@mui/material";
 import { AuthContainer } from "./AuthContainer";
 
 export interface DialogProps {
@@ -20,11 +12,10 @@ export interface DialogProps {
 }
 
 export default function AuthDialog() {
-  //const { data } = useSession();
   const [open, setOpen] = React.useState(false);
 
   const handleToggleAuthMenu = () => {
-    setOpen((prev) => !prev);
+    setOpen(prev => !prev);
   };
 
   return (
@@ -33,9 +24,7 @@ export default function AuthDialog() {
         <IconButton
           aria-label="account of current user"
           onClick={handleToggleAuthMenu}
-          sx={{ p: 0 }}
-        >
-          {/*<Avatar sx={{ backgroundColor: alpha("#fff", 0.25) }} />*/}
+          sx={{ p: 0 }}>
           <AccountCircleIcon sx={{ color: "#b2b2b2" }} />
         </IconButton>
       </Tooltip>
