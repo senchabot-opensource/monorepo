027132fb0e09b8e8c25b11ab82f1de6917aab2e3
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-04-18 14:46:25 +0300

    feat(apps/discord-bot): Discord bot create scheduled event command initial

--- apps/discord-bot/commands/event.ts
+++ apps/discord-bot/commands/event.ts
@@ -3,13 +3,37 @@ import {
   Guild,
   PermissionFlagsBits,
   SlashCommandBuilder,
+  SlashCommandNumberOption,
+  SlashCommandStringOption,
   SlashCommandSubcommandBuilder,
 } from "discord.js";
 import { checkMemberPermission } from "../utils/memberFunctions";
 
 const manageEventsPermFlag = PermissionFlagsBits.ManageEvents;
 
-const purgeCommand = new SlashCommandSubcommandBuilder()
+const eventCreateCommand = new SlashCommandSubcommandBuilder()
+  .setName("create")
+  .addStringOption((option: SlashCommandStringOption) =>
+    option
+      .setName("event_name")
+      .setDescription("Event name.")
+      .setRequired(true),
+  )
+  .addNumberOption((option: SlashCommandNumberOption) =>
+    option
+      .setName("event_start_date")
+      .setDescription("Event start time.")
+      .setRequired(true),
+  )
+  .addStringOption((option: SlashCommandStringOption) =>
+    option
+      .setName("event_location")
+      .setDescription("Event Location.")
+      .setRequired(false),
+  )
+  .setDescription("Create scheduled guild events");
+
+const eventPurgeCommand = new SlashCommandSubcommandBuilder()
   .setName("purge")
   .setDescription("Purge scheduled guild events");
 
@@ -17,7 +41,8 @@ export default {
   data: new SlashCommandBuilder()
     .setName("event")
     .setDescription("Manage guild events")
-    .addSubcommand(purgeCommand)
+    .addSubcommand(eventCreateCommand)
+    .addSubcommand(eventPurgeCommand)
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageEvents),
 
   async execute(interaction: ChatInputCommandInteraction) {
@@ -26,18 +51,45 @@ export default {
     if (
       !checkMemberPermission(
         interaction.memberPermissions,
-        manageEventsPermFlag
+        manageEventsPermFlag,
       )
     ) {
       console.log(
-        `MEMBER "${interaction.member?.user.username}" DOES NOT HAVE "MANAGE EVENTS" PERMISSION.`
+        `MEMBER "${interaction.member?.user.username}" DOES NOT HAVE "MANAGE EVENTS" PERMISSION.`,
       );
       return;
     }
 
+    if (interaction.options.getSubcommand() === "create") {
+      const eventNameString = interaction.options.getString("event_name");
+      const eventStartDateNumber =
+        interaction.options.getNumber("event_start_date");
+
+      if (!eventStartDateNumber || !eventNameString) return;
+      const eventLocationtring =
+        interaction.options.getString("event_location");
+
+      let location = "";
+
+      if (eventLocationtring) {
+        location = eventLocationtring;
+      }
+
+      const eventOptions = {
+        name: eventNameString,
+        scheduledStartTime: eventStartDateNumber,
+        scheduledEndTime: eventStartDateNumber,
+        privacyLevel: 2,
+        entityType: 3,
+        entityMetadata: { location: location },
+      };
+      const event = guild.scheduledEvents.create(eventOptions);
+      event.then(e => e.setLocation(location));
+    }
+
     if (interaction.options.getSubcommand() === "purge") {
       const events = await guild.scheduledEvents.fetch();
-      events?.forEach((event) => event.delete());
+      events?.forEach(event => event.delete());
 
       await interaction.reply({
         content: "All scheduled events deleted.",
