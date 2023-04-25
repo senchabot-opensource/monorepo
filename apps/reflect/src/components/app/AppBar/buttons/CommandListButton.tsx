dcea566437c78aea9b9dd9ca66907d24f09786d9
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-04-25 06:01:11 +0300

    feat(apps/reflect): add CommandListButton to AppBar

---
+++ apps/reflect/src/components/app/AppBar/buttons/CommandListButton.tsx
@@ -0,0 +1,24 @@
+import { IconButton, Typography } from "@mui/material";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import Link from "next/link";
+import { BootstrapTooltip } from "../../../Tooltip";
+
+const CommandListButton = () => {
+  return (
+    <BootstrapTooltip title="Bot Command List">
+      <Typography>
+        <Link href="/app/command-list" passHref>
+          <IconButton
+            aria-label="bot command list"
+            sx={{
+              display: "flex",
+            }}>
+            <FormatListBulletedIcon />
+          </IconButton>
+        </Link>
+      </Typography>
+    </BootstrapTooltip>
+  );
+};
+
+export default CommandListButton;
