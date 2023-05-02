07b4fcee12702f007847147d863cc7d135b09aad
Rasit <12167194+durmusrasit@users.noreply.github.com>
2023-05-02 23:08:13 +0300

    update(apps/twitch-bot): add old string templates (#92)
    add old string templates for backward compatibility

--- apps/twitch-bot/internal/command/helpers/helpers.go
+++ apps/twitch-bot/internal/command/helpers/helpers.go
@@ -26,6 +26,10 @@ func FormatCommandContent(commandData *models.BotCommand, message twitch.Private
 		"{random_number}": strconv.Itoa(rand.Intn(max-min) + min),
 		"{date}":          message.Time.Format(dateTemplate),
 		"{cmd.date}":      commandData.CreatedAt.Format(dateTemplate),
+
+		// we will keep these old string templates used in commands for a while for backward compatibility.
+		"{user_name}": userName,
+		"{cmd_date}":  commandData.CreatedAt.Format(dateTemplate),
 	}
 
 	for k, v := range stringTemplates {
