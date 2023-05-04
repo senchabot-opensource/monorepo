62936737ded36e9bc3f04b122373959bdbbfc74a
Rasit <12167194+durmusrasit@users.noreply.github.com>
2023-05-04 16:33:14 +0300

    feat(apps/reflect): disable save button when no change is made to the form (#106)

--- apps/reflect/src/forms/TwitchBotForm.tsx
+++ apps/reflect/src/forms/TwitchBotForm.tsx
@@ -18,6 +18,7 @@ import { ITwitchBotFormSubmitData } from "src/types";
 const TwitchBotForm = () => {
   const [alertIsOpen, setAlertIsOpen] = useState<boolean>(false);
   const [snackbarIsOpen, setSnackbarIsOpen] = useState<boolean>(false);
+  const [buttonEnabled, setButtonEnabled] = useState<boolean>(false);
 
   const { data: botActivityEnabledConfig, isLoading } =
     trpc.twitchBot.getConfig.useQuery({
@@ -45,6 +46,7 @@ const TwitchBotForm = () => {
   const configsMutate = trpc.twitchBot.setConfig.useMutation({
     onSuccess() {
       setSnackbarIsOpen(true);
+      setButtonEnabled(false);
     },
     onError() {
       setAlertIsOpen(true);
@@ -89,7 +91,7 @@ const TwitchBotForm = () => {
           <Controller
             name="botActivityEnabled"
             control={control}
-            render={({ field }) => (
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
               <FormControl
                 sx={{ m: 2 }}
                 size="small"
@@ -100,20 +102,27 @@ const TwitchBotForm = () => {
                 </InputLabel>
 
                 <Select
-                  {...field}
+                  onChange={field => {
+                    onChange(field.target.value);
+                    setButtonEnabled(true);
+                  }}
+                  value={value}
                   labelId="select-botActivityEnabled"
                   id="select-botActivityEnabled"
                   label="Twitch Bot Activities">
                   <MenuItem value="0">Disabled</MenuItem>
                   <MenuItem value="1">Enabled</MenuItem>
                 </Select>
-                <FormHelperText>
-                  {handleError(errors.botActivityEnabled)}
-                </FormHelperText>
+                <FormHelperText>{handleError(error)}</FormHelperText>
               </FormControl>
             )}
           />
-          <Button fullWidth variant="outlined" sx={{ m: 1 }} type="submit">
+          <Button
+            disabled={!buttonEnabled}
+            fullWidth
+            variant="outlined"
+            sx={{ m: 1 }}
+            type="submit">
             Save
           </Button>
         </Box>
