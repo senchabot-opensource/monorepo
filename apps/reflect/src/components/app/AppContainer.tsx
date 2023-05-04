7a79ea8727ed6d7f51726e95087b5718159a13de
Mert <88425310+mertsabinov@users.noreply.github.com>
2023-05-04 16:27:13 +0300

    refactor(app/reflect): sign in & login components (#107)
    * add: auth login button
    
    * refactor: auth container
    
    * refactor: AppContainer dialog
    
    * add: border radius
    
    * add: start icon font size
    
    * refactor: auth login button
    
    * update: padding
    
    ---------
    
    Co-authored-by: Rasit <12167194+durmusrasit@users.noreply.github.com>

--- apps/reflect/src/components/app/AppContainer.tsx
+++ apps/reflect/src/components/app/AppContainer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, CssBaseline, Paper, Dialog } from "@mui/material";
+import { Container, CssBaseline, Paper, Dialog, DialogContent } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import ResponsiveAppBar from "./AppBar";
 
@@ -9,6 +9,7 @@ import { useSession } from "next-auth/react";
 import { AuthContainer } from "../auth/AuthContainer";
 import Loading from "../loading/Loading";
 import VersionText from "../common/VersionText";
+import { DialogBody } from "next/dist/client/components/react-dev-overlay/internal/components/Dialog";
 
 export interface IAppContainer {
   isLoading: boolean;
@@ -45,8 +46,26 @@ export default function AppContainer({ isLoading, children }: IAppContainer) {
       ) : (
         !isLoading &&
         isAuthLoading !== "loading" && (
-          <Dialog open={true}>
-            <AuthContainer />
+          <Dialog
+            fullScreen={true}
+            open={true}
+            PaperProps={{
+              elevation: 0,
+              style: {
+                backgroundColor: "black",
+                boxShadow: "none",
+                borderRadius: "8px",
+                overflow: "hidden",
+                height: "fit-content",
+                width: "325px",
+              },
+            }}>
+            <DialogContent
+              sx={{
+                padding: 2,
+              }}>
+              <AuthContainer />
+            </DialogContent>
           </Dialog>
         )
       )}
