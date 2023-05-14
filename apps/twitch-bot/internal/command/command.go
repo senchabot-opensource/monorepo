1c6ee4efa26263d5b01cc03eefab1dca9352922f
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-05-14 14:03:30 +0300

    refactor(apps/twitch-bot): create splitMessage function and rename variable names

--- apps/twitch-bot/internal/command/command.go
+++ apps/twitch-bot/internal/command/command.go
@@ -35,16 +35,65 @@ func GetCommands() map[string]func(client *client.Clients, server *server.Sencha
 	return commands
 }
 
+func splitMessage(message string) (string, []string) {
+	var splitMsg = strings.Split(message, " ")
+	var cmdName = strings.Trim(splitMsg[0], " ")
+	var params = splitMsg[1:]
+
+	if !strings.HasPrefix(cmdName, "!") {
+		return "", nil
+	}
+
+	cmdName = strings.TrimPrefix(cmdName, "!")
+
+	return cmdName, params
+}
+
 func RunCommand(context context.Context, client *client.Clients, server *server.SenchabotAPIServer, message twitch.PrivateMessage) {
 	commands := GetCommands()
 
-	var splitMsg = strings.Split(message.Message, " ")
-	var cmd = strings.Trim(splitMsg[0], " ")
-	var params = splitMsg[1:]
-	if strings.HasPrefix(cmd, "!") {
-		cmd = strings.TrimPrefix(cmd, "!")
-		if c, ok := commands[cmd]; ok {
-			c(client, server, message, cmd, params)
+	cmdName, params := splitMessage(message.Message)
+	if cmdName == "" {
+		return
+	}
+
+	if c, ok := commands[cmdName]; ok {
+		c(client, server, message, cmdName, params)
+		configData, err := server.GetTwitchBotConfig(context, message.RoomID, "bot_activity_enabled")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		if configData != nil {
+			if configData.Value == "1" {
+				if err := server.CreateBotActionActivity(context, "twitch", cmdName, message.RoomID, message.User.DisplayName); err != nil {
+					fmt.Println(err.Error())
+				}
+			}
+		}
+		return
+	}
+
+	// HANDLE CUSTOM COMMANDS
+	commandAlias, cmdAliasErr := server.GetCommandAlias(context, cmdName, message.RoomID)
+	if cmdAliasErr != nil {
+		fmt.Println(cmdAliasErr.Error())
+	}
+
+	if commandAlias != nil {
+		cmdName = *commandAlias
+	}
+
+	cmdData, err := server.GetBotCommand(context, cmdName, message.RoomID)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if cmdData != nil {
+		if message.RoomID == cmdData.TwitchChannelID {
+			formattedCommandContent := helpers.FormatCommandContent(cmdData, message)
+			client.Twitch.Say(message.Channel, formattedCommandContent)
 			configData, err := server.GetTwitchBotConfig(context, message.RoomID, "bot_activity_enabled")
 			if err != nil {
 				fmt.Println(err.Error())
@@ -52,44 +101,8 @@ func RunCommand(context context.Context, client *client.Clients, server *server.
 
 			if configData != nil {
 				if configData.Value == "1" {
-					if err := server.CreateBotActionActivity(context, "twitch", cmd, message.RoomID, message.User.DisplayName); err != nil {
+					if err := server.CreateBotActionActivity(context, "twitch", cmdName, message.RoomID, message.User.DisplayName); err != nil {
 						fmt.Println(err.Error())
-					}
-				}
-			}
-			return
-		}
-
-		// HANDLE CUSTOM COMMANDS
-		commandAlias, cmdAliasErr := server.GetCommandAlias(context, cmd, message.RoomID)
-		if cmdAliasErr != nil {
-			fmt.Println(cmdAliasErr.Error())
-		}
-
-		if commandAlias != nil {
-			cmd = *commandAlias
-		}
-
-		cmdData, err := server.GetBotCommand(context, cmd, message.RoomID)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		if cmdData != nil {
-			if message.RoomID == cmdData.TwitchChannelID {
-				formattedCommandContent := helpers.FormatCommandContent(cmdData, message)
-				client.Twitch.Say(message.Channel, formattedCommandContent)
-				configData, err := server.GetTwitchBotConfig(context, message.RoomID, "bot_activity_enabled")
-				if err != nil {
-					fmt.Println(err.Error())
-				}
-
-				if configData != nil {
-					if configData.Value == "1" {
-						if err := server.CreateBotActionActivity(context, "twitch", cmd, message.RoomID, message.User.DisplayName); err != nil {
-							fmt.Println(err.Error())
-						}
 					}
 				}
 			}
