4408c6b2e9ac8381998eb12ea291520db65abb2c
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-06-08 16:23:59 +0300

    feat(apps/twitch-bot): add help command

---
+++ apps/twitch-bot/internal/command/help.go
@@ -0,0 +1,20 @@
+package command
+
+import (
+	"strings"
+
+	"github.com/gempir/go-twitch-irc/v3"
+)
+
+func (s *commands) HelpCommand(message twitch.PrivateMessage, commandName string, params []string) {
+	commandListMap := s.GetCommands()
+	var commmandList []string
+
+	for k := range commandListMap {
+		commmandList = append(commmandList, k)
+	}
+
+	commandListString := strings.Join(commmandList, ", ")
+
+	s.client.Twitch.Say(message.Channel, "Available Commands: "+commandListString)
+}
