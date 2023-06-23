e1c52088499b04e3ddf816b593c2e6a24712f50d
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-06-23 20:01:08 +0300

    feat(apps/twitch-bot): add TestParseMessage function

--- apps/twitch-bot/internal/command/helpers/helpers_test.go
+++ apps/twitch-bot/internal/command/helpers/helpers_test.go
@@ -6,6 +6,74 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestParseMessage(t *testing.T) {
+	type TestCase struct {
+		description string
+		input       string
+		expected    string
+	}
+
+	testCases := []TestCase{
+		{
+			description: "with just message",
+			input:       "abc",
+			expected:    "",
+		},
+		{
+			description: "with a command",
+
+			input:    "!kampus",
+			expected: "discord.gg/kampus • github.com/kamp-us",
+		},
+		{
+
+			description: "with a mention and a command",
+			input:       "@senchabot !kampus",
+			expected:    "discord.gg/kampus • github.com/kamp-us",
+		},
+		{
+
+			description: "with a command and a mention",
+			input:       "!kampus @senchabot",
+			expected:    "discord.gg/kampus • github.com/kamp-us @senchabot",
+		},
+		{
+
+			description: "with wrong mention",
+			input:       "@s",
+			expected:    "",
+		},
+		{
+
+			description: "with just mention",
+			input:       "@senchabot",
+			expected:    "",
+		},
+		//{
+
+		//	description: "with a command and its params",
+		//	input:       "!acmd acommand a command content",
+		//	expected:    "",
+		//},
+
+		//{
+
+		//	description: "with a command and params but some of params are just space",
+		//	input:       "!acmd  a couple of params",
+		//	expected:    "",
+		//},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+
+			// test params cannot be a space or empty value
+			// expectedCmd, expectedParams = ParseMessage(testCase.input)
+			ParseMessage(testCase.input)
+		})
+	}
+}
+
 func TestMakeUniqueArray(t *testing.T) {
 	type TestCase struct {
 		description string
