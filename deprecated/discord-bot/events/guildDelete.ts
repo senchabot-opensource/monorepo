91bf859b3ff572c6299cec082a362e07081c95dc
senchabot-app <94869947+senchabot-app@users.noreply.github.com>
2023-07-24 23:47:23 +0300

    delete deprecated bot files

--- deprecated/discord-bot/events/guildDelete.ts
+++
@@ -1,9 +0,0 @@
-import { Guild } from "discord.js";
-import { deleteDiscordServerFromDB } from "../db/functions";
-
-export default {
-  name: "guildDelete",
-  execute(guild: Guild) {
-    deleteDiscordServerFromDB(guild);
-  },
-};
