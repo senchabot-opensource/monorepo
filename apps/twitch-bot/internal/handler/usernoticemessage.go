1353c3ca746e8b6244e706e737e13823525cf3d0
J <119960643+jhoo-cpp@users.noreply.github.com>
2024-01-07 12:24:02 +0300

    fix import in usernoticemessage.go (#422)

--- apps/twitch-bot/internal/handler/usernoticemessage.go
+++ apps/twitch-bot/internal/handler/usernoticemessage.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 
 	"github.com/gempir/go-twitch-irc/v3"
-	"github.com/senchabot-dev/monorepo/apps/twitch-bot/client"
-	"github.com/senchabot-dev/monorepo/apps/twitch-bot/internal/service"
+	"github.com/senchabot-opensource/monorepo/apps/twitch-bot/client"
+	"github.com/senchabot-opensource/monorepo/apps/twitch-bot/internal/service"
 )
 
 var subTypes = []string{"sub", "resub", "subgift", "submysterygift"}
