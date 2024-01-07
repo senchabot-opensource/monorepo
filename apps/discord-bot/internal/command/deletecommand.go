19c52844d22601a9751d75ffc36193802b17679e
J <119960643+jhoo-cpp@users.noreply.github.com>
2024-01-07 19:24:05 +0300

    add permission to discord commands (#427)

--- apps/discord-bot/internal/command/deletecommand.go
+++ apps/discord-bot/internal/command/deletecommand.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"context"
+	"errors"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/senchabot-opensource/monorepo/command"
@@ -12,6 +13,15 @@ func (c *commands) DeleteCommandCommand(context context.Context, m *discordgo.Me
 	msgData := &models.MessageData{
 		PlatformEntityID: m.GuildID,
 		UserName:         m.Author.Username,
+	}
+
+	p, err := c.dS.UserChannelPermissions(m.Author.ID, m.ChannelID)
+	if err != nil {
+		return nil, err
+	}
+
+	if p&discordgo.PermissionManageChannels != discordgo.PermissionManageChannels {
+		return nil, errors.New("dont have permission")
 	}
 
 	return command.DcmdCommand(context, c.service.DeleteCommand, c.IsSystemCommand, *msgData, commandName, params)
