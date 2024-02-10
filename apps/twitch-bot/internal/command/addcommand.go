6190deb11a24b7322c51fa5641b2d2bece2a0b1d
barbequeorbarbecue <151953193+barbequeorbarbecue@users.noreply.github.com>
2024-02-10 21:42:44 +0300

    refactor: add CannotExecuteCommand variable for all (#432)

--- apps/twitch-bot/internal/command/addcommand.go
+++ apps/twitch-bot/internal/command/addcommand.go
@@ -7,12 +7,13 @@ import (
 	"github.com/gempir/go-twitch-irc/v3"
 	"github.com/senchabot-opensource/monorepo/apps/twitch-bot/internal/command/helpers"
 	"github.com/senchabot-opensource/monorepo/command"
+	"github.com/senchabot-opensource/monorepo/config"
 	"github.com/senchabot-opensource/monorepo/packages/gosenchabot/models"
 )
 
 func (c *commands) AddCommandCommand(context context.Context, message twitch.PrivateMessage, commandName string, params []string) (*models.CommandResponse, error) {
 	if !helpers.CanExecuteCommand(context, c.service, message.Tags["badges"], message.RoomID) {
-		return nil, errors.New(message.User.DisplayName + " cannot execute the command")
+		return nil, errors.New(message.User.DisplayName + config.CannotExecuteCommand)
 	}
 
 	msgData := &models.MessageData{
