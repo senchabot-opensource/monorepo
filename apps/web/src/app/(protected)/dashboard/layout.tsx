89f099151d5413a9534cf72774ec7f14b984a7a5
eckoln <ahmettongul01@gmail.com>
2024-05-12 14:48:17 +0300

    feat(apps/web): add `ui/sonner`

--- apps/web/src/app/(protected)/dashboard/layout.tsx
+++ apps/web/src/app/(protected)/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { Sidebar } from '@/components/pages/dashboard/sidebar'
+import { Toaster } from '@/components/ui/sonner'
 
 interface Props {
   children: React.ReactNode
@@ -9,6 +10,7 @@ export default function DashboardLayout({ children }: Props) {
     <div className="relative mx-auto flex h-screen w-full max-w-screen-xl">
       <Sidebar />
       <main className="ml-64 grow px-8 py-6">{children}</main>
+      <Toaster position="top-center" duration={1500} />
     </div>
   )
 }
