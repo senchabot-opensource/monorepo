36064e9e838bdd447a0acae6e03a497b79d111ee
eckoln <ahmettongul01@gmail.com>
2024-05-26 15:13:39 +0300

    refactor(apps/web): `landing/Features` changes

--- apps/web/src/components/pages/landing/features.tsx
+++ apps/web/src/components/pages/landing/features.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle2Icon, XIcon } from 'lucide-react'
+import { CheckCircleIcon, XIcon } from 'lucide-react'
 
 export function Features() {
   return (
@@ -7,16 +7,16 @@ export function Features() {
         <ul className="grid grid-cols-1 justify-center gap-4 py-8 text-3xl font-medium lg:grid-cols-3 lg:text-4xl">
           <li className="inline-flex items-center justify-center space-x-4 lg:justify-start">
             <XIcon className="size-12 shrink-0 text-red-500" />
-            <p className="text-muted-foreground">Payment</p>
-          </li>
-          <li className="inline-flex items-center justify-center space-x-4 lg:justify-start">
-            <XIcon className="size-12 shrink-0 text-red-500" />
             <p className="text-nowrap text-muted-foreground lg:text-wrap">
               Download required
             </p>
           </li>
           <li className="inline-flex items-center justify-center space-x-4 lg:justify-start">
-            <CheckCircle2Icon className="size-12 shrink-0 text-green-500" />
+            <CheckCircleIcon className="size-12 shrink-0 text-green-500" />
+            <p>Free</p>
+          </li>
+          <li className="inline-flex items-center justify-center space-x-4 lg:justify-start">
+            <CheckCircleIcon className="size-12 shrink-0 text-green-500" />
             <p>Open-source</p>
           </li>
         </ul>
