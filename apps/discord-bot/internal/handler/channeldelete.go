3e1d69277360790268f7df9a11397283d2a1a852
barbequeorbarbecue <151953193+barbequeorbarbecue@users.noreply.github.com>
2024-09-08 12:03:29 +0300

    feat(apps/discord-bot): add category filter and announcement channel delete to channel delete handler

--- apps/discord-bot/internal/handler/channeldelete.go
+++ apps/discord-bot/internal/handler/channeldelete.go
@@ -17,5 +17,21 @@ func (h *handler) ChannelDelete() {
 		if ok {
 			log.Println("Livestream announcements deleted for the channel `" + c.Name + "` in guild id `" + c.GuildID + "`")
 		}
+
+		ok, err = h.service.DeleteDiscordChannelTwitchCategoryFilter(context.Background(), c.GuildID, c.ID)
+		if err != nil {
+			log.Println("[handler.ChannelDelete] DeleteDiscordChannelTwitchCategoryFilter error:", err.Error())
+		}
+		if ok {
+			log.Println("Twitch category filters deleted for the channel `" + c.Name + "` in guild id `" + c.GuildID + "`")
+		}
+
+		ok, err = h.service.DeleteAnnouncementChannel(context.Background(), c.ID)
+		if err != nil {
+			log.Println("[handler.ChannelDelete] DeleteAnnouncementChannel error:", err.Error())
+		}
+		if ok {
+			log.Println("Announcement channel data deleted for the channel `" + c.Name + "` in guild id `" + c.GuildID + "`")
+		}
 	})
 }
