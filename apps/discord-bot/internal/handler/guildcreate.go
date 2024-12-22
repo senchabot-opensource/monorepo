ecc099403eeccdea9a1c353973199d96b51258c4
barbequeorbarbecue <151953193+barbequeorbarbecue@users.noreply.github.com>
2024-12-22 17:15:48 +0300

    feat(discord-bot): add missing logs for GuildCreate and GuildDelete handlers (#523)

--- apps/discord-bot/internal/handler/guildcreate.go
+++ apps/discord-bot/internal/handler/guildcreate.go
@@ -11,10 +11,12 @@ import (
 func (h *handler) GuildCreate() {
 	ctx := context.Background()
 	h.discordClient.AddHandler(func(s *discordgo.Session, g *discordgo.GuildCreate) {
+		log.Println("[handler.GuildCreate] Adding the guild `" + g.Name + "` (ID: " + g.ID + ") to the database")
 		err := h.service.AddServerToDB(ctx, g.ID, g.Name, g.OwnerID)
 		if err != nil {
-			log.Println("[GuildCreate] AddServerToDB error:", err.Error())
+			log.Println("[handler.GuildCreate] AddServerToDB error:", err.Error())
 		}
+		log.Println("[handler.GuildCreate] Initiate Twitch live streaming checks for the guild `" + g.Name + "` (ID: " + g.ID + ")")
 		streamer.StartCheckLiveStreams(s, ctx, h.service, g.ID)
 	})
 }
--- apps/discord-bot/internal/handler/guilddelete.go
+++ apps/discord-bot/internal/handler/guilddelete.go
@@ -11,15 +11,16 @@ import (
 func (h *handler) GuildDelete() {
 	ctx := context.Background()
 	h.discordClient.AddHandler(func(s *discordgo.Session, g *discordgo.GuildDelete) {
+		log.Println("[handler.GuildDelete] Delete records for the guild `" + g.Name + "` (ID: " + g.ID + ")")
 		err := h.service.DeleteServerFromDB(ctx, g.ID)
 		if err != nil {
-			log.Println("[GuildDelete] DeleteServerFromDB error:", err.Error())
+			log.Println("[handler.GuildDelete] DeleteServerFromDB error:", err.Error())
 		}
 		streamer.StopCheckLiveStreams(g.ID)
 		streamer.DeleteServerFromData(g.ID)
 		_, err = h.service.DeleteDiscordTwitchLiveAnnosByGuildId(ctx, g.ID)
 		if err != nil {
-			log.Println("[GuildDelete] DeleteDiscordTwitchLiveAnnosByGuildId error:", err.Error())
+			log.Println("[handler.GuildDelete] DeleteDiscordTwitchLiveAnnosByGuildId error:", err.Error())
 		}
 	})
 }
