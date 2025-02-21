d1bbc785b263bb14cb1dacc0f711a2c5a7e530b7
barbequeorbarbecue <151953193+barbequeorbarbecue@users.noreply.github.com>
2025-02-21 22:33:34 +0300

    feat(discord-bot): add English good morning reaction support (#544)
    * feat(discord-bot): add English good morning reaction support
    
    * feat(discord-bot): add user privacy check to MessageReactionAdd

--- apps/discord-bot/internal/handler/messagereactionadd.go
+++ apps/discord-bot/internal/handler/messagereactionadd.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"context"
 	"log"
 	"regexp"
 
@@ -9,13 +10,22 @@ import (
 
 func (h *handler) MessageReactionAdd() {
 	h.discordClient.AddHandler(func(s *discordgo.Session, i *discordgo.MessageReactionAdd) {
+		userPrivacyPreferences, err := h.service.GetDiscordUserPrivacyPreferences(context.Background(), i.UserID)
+		if err != nil {
+			log.Println("[handler.MessageReactionAdd] service.GetDiscordUserPrivacyPreferences error:", err.Error())
+			return
+		}
+		if userPrivacyPreferences != nil && userPrivacyPreferences.DoNotTrackMessages {
+			return
+		}
+
 		msg, err := s.ChannelMessage(i.ChannelID, i.MessageID)
 		if err != nil {
 			log.Println("[handler.MessageReactionAdd] ChannelMessage error:", err.Error())
 			return
 		}
 
-		goodMorningRegexp := regexp.MustCompile(`(?i)g(ü|u)nayd(ı|i)`)
+		goodMorningRegexp := regexp.MustCompile(`(?i)(g(ü|u)nayd(ı|i)|good\s*morn[i]+ng+)`)
 		if goodMorningRegexp.MatchString(msg.Content) && i.Emoji.Name == "🌞" {
 			err = s.MessageReactionAdd(msg.ChannelID, msg.ID, "🌞")
 			if err != nil {
