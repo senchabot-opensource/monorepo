7c771d2e3296e2ccf9de743498694c7a61a4b38d
rasitds <12167194+rasitds@users.noreply.github.com>
2026-02-24 16:14:21 +0300

    feat: pass discordClientId to JoinableEntities component

--- apps/web/src/app/(protected)/dashboard/settings/@tabs/servers/joinable-entities-list.tsx
+++ apps/web/src/app/(protected)/dashboard/settings/@tabs/servers/joinable-entities-list.tsx
@@ -1,8 +1,10 @@
 import { getUserEntities } from '@/services/queries/users'
 
+import { env } from '@/config/env'
+
 import { EntitiesList } from './entities-list'
 
 export async function JoinableEntities() {
   const entities = await getUserEntities('not_joined')
-  return <EntitiesList entities={entities} />
+  return <EntitiesList entities={entities} discordClientId={env.AUTH_DISCORD_ID} />
 }
